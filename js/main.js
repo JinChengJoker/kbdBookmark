f68450a90eb2c2b6b3baf312c49c1b9f4f577570
JinCheng <595580375@qq.com>
2018-03-31 23:11:39 +0800

    封装函数

--- js/main.js
+++ js/main.js
@@ -1,63 +1,12 @@
-var keys = [
-    ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
-    ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
-    ['z', 'x', 'c', 'v', 'b', 'n', 'm']
-]
+// 1、初始化数据 
+var keyData = initData()
+var keys = keyData.keys
+var urls = keyData.urls
 
-var urls = JSON.parse(localStorage.getItem('urls')) || {
-    q: 'www.tencent.com',
-    w: 'www.weibo.com',
-    e: 'www.ele.me',
-    r: 'www.renren.com',
-    t: 'www.tmall.com',
-    y: 'www.youku.com',
-    u: 'www.uber.com.cn',
-    i: 'www.iqiyi.com',
-    o: 'www.oppo.com',
-    p: 'www.pixabay.com'
-}
+// 2、生成键盘
+createKeyboard()
 
-for(var i = 0; i < keys.length; i++) {
-    var row = document.createElement('div')
-    row.className = 'row'
-    rows.appendChild(row)
-    for(var k = 0; k < keys[i].length; k++) {
-        var letter = keys[i][k]
-        var kbd = document.createElement('kbd')
-        var span = document.createElement('span')
-        span.textContent = letter
-        kbd.appendChild(span)
-        row.appendChild(kbd)
-        var btn = document.createElement('button')
-        btn.id = letter
-        btn.textContent = '编辑'
-        kbd.appendChild(btn)
-        btn.onclick = function(e) {
-            var key = e.target.id
-            var newUrl = prompt('请输入新的网址：')
-            if(newUrl) {
-                var img = e.target.nextSibling
-                img.src = '//' + newUrl + '/favicon.ico'
-                img.onerror = function(e) {
-                    e.target.src = './images/ico_heart.png'
-                }
-                urls[key] = newUrl
-                localStorage.setItem('urls', JSON.stringify(urls))
-            }
-        }
-        var img = document.createElement('img')
-        if(urls[letter]) {
-            img.src = '//' + urls[letter] + '/favicon.ico'
-            img.onerror = function(e) {
-                e.target.src = './images/ico_heart.png'
-            }
-        } else {
-            img.src = './images/ico_point.png'
-        }
-        kbd.appendChild(img)
-    }
-}
-
+// 3、监听按键打开对应网页
 document.onkeypress = function(kbd) {
     var key = kbd.key
     var url = urls[key]
@@ -65,5 +14,92 @@ document.onkeypress = function(kbd) {
         window.open('http://' + url, '_blank')
     } else {
         alert('未定义的书签')
+    }
+}
+
+// 工具函数
+function initData() {
+    var keys = [
+        ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+        ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+        ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+    ]
+    var urls = JSON.parse(localStorage.getItem('urls')) || {
+        q: 'www.tencent.com',
+        w: 'www.weibo.com',
+        e: 'www.ele.me',
+        t: 'www.tmall.com',
+        u: 'www.uber.com.cn',
+        i: 'www.iqiyi.com',
+        o: 'www.oppo.com',
+        p: 'www.pixabay.com'
+    }
+    return {
+        keys, urls
+    }
+}
+
+function createKeyboard() {
+    for(var i = 0; i < keys.length; i++) {
+        var row = tag('div', {
+            className: 'row'
+        })
+        rows.appendChild(row)
+        for(var k = 0; k < keys[i].length; k++) {
+            var letter = keys[i][k]
+            var kbd = tag('kbd')
+            var span = tag('span', {
+                textContent: letter
+            })
+            var btn = createEditButton(letter)
+            var ico = createWebsiteIco(letter)
+            kbd.appendChild(span)
+            kbd.appendChild(btn)
+            kbd.appendChild(ico)
+            row.appendChild(kbd)
+        }
+    }
+}
+
+function tag(tagName, attributes) {
+    var tag = document.createElement(tagName)
+    for(var key in attributes) {
+        tag[key] = attributes[key]
+    }
+    return tag
+}
+
+function createEditButton(letter) {
+    var btn = tag('button', {
+        id: letter,
+        textContent: '编辑'
+    })
+    btn.onclick = function(e) {
+        var key = e.target.id
+        var newUrl = prompt('请输入新的网址：')
+        if(newUrl) {
+            var img = e.target.nextSibling
+            setImgSrc(img, newUrl)
+            urls[key] = newUrl
+            localStorage.setItem('urls', JSON.stringify(urls))
+        }
+    }
+    return btn
+}
+
+function createWebsiteIco(letter) {
+    var ico = tag('img')
+    if(urls[letter]) {
+        setImgSrc(ico, urls[letter])
+    } else {
+        ico.src = './images/ico_point.png'
+    }
+    return ico
+}
+
+function setImgSrc(img, url) {
+    img.src = '//' + url + '/favicon.ico'
+    img.onerror = function(e) {
+        e.target.src = './images/ico_heart.png'
     }
 }
